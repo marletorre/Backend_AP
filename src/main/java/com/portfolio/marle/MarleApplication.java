6b3ad2fb1e5c160230adcb8f672fc56a563ddbb9
Marlene <marlene.torrealbaf@gmail.com>
2022-10-31 19:13:35 +0100

    se actualiza url del front

--- src/main/java/com/portfolio/marle/MarleApplication.java
+++ src/main/java/com/portfolio/marle/MarleApplication.java
@@ -19,7 +19,7 @@ public class MarleApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://mtfrontap.web.app/"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://frontmarleap.web.app/"));
 
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
